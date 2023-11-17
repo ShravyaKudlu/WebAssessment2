cf6e012192ac82987921f22fce42ab1c7541a6a9
ShravyaKudlu <shravyakshanbhogue@gmail.com>
2023-11-16 19:52:01 -0500

    fixes

--- index.js
+++ index.js
@@ -23,11 +23,37 @@ function checkPalindrome(num) {
 }
 
 function displayResult(isPalindrome) {
-    var resultElement = document.getElementById('result');
+    var resultElement = document.getElementById('result2');
     
     if (isPalindrome) {
         resultElement.textContent = "The input is a palindrome!";
     } else {
         resultElement.textContent = "The input is not a palindrome.";
     }
+}
+
+document.getElementById('reverseButton').addEventListener('click', function() {
+    var inputString = document.getElementById('inputString').value;
+    var reversedString = reverseString(inputString);
+    document.getElementById('result1').textContent = reversedString;
+});
+
+function reverseString(str) {
+    let strRev = "";
+    for(let x=str.length-1;x>=0;x--){
+        strRev = strRev + str[x];
+    }
+    return strRev;
+}
+
+document.getElementById('calculateButton').addEventListener('click', function() {
+    var subtotal = parseFloat(document.getElementById('subtotal').value);
+    var tipPercentage = parseFloat(document.getElementById('tipPercentage').value);
+    var totalAmount = calculateTotal(subtotal, tipPercentage);
+    document.getElementById('totalAmount').textContent = "Total Amount: $" + totalAmount.toFixed(2);
+});
+
+function calculateTotal(subtotal, tipPercentage) {
+    var tipAmount = (subtotal * tipPercentage) / 100;
+    return subtotal + tipAmount;
 }
